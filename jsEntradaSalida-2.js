3bb7ac35ae230753423346a3ed716efb556d3fc5
marianomadou <marianomadou@gmail.com>
2016-11-14 18:55:33 -0300

    Clase 1 - 14/11/2016

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,23 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	/* 
+	concepto de variable 
+	concatenacion
+	prompt
+
+	*/
+	//alert muestra el contenido de la variable
+	var producto;
+
+	producto ="samsung";
+	producto=prompt("ingrese marca", "sin marca");
+	var precio=prompt("ingrese precio", "999");
+
+	alert("su producto es: "+producto+" y su precio es: "+precio);
+
+}
+
+
+
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,14 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+	
+//id="elNombre"
+
+var dat= prompt("elNombre");
+document.getElementById('elNombre').value=dat;
+
+}
+
